ebdca563a5d9e274b3334d05c2527114abf41c4b
Andy15084 <your-email@example.com>
2025-03-31 22:31:53 +0200

    Fix: Update database client configuration for Prisma Accelerate

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof prismaClientWithExtensions> | undefined
 }
 
-export const prisma = globalThis.prisma || new PrismaClient().$extends(withAccelerate())
+const prismaClientWithExtensions = () => {
+  return new PrismaClient().$extends(withAccelerate())
+}
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma 
+export const prisma = globalForPrisma.prisma ?? prismaClientWithExtensions()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
