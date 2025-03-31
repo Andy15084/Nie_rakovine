0e7c62e222dad474d8afbb2b818146eb45dfa305
Andy15084 <your-email@example.com>
2025-03-31 21:45:25 +0200

    Update: Use direct connection strings for database

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,12 @@
-import { PrismaClient } from '@prisma/client/edge'
+import { PrismaClient } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma || new PrismaClient()
+const prisma = globalThis.prisma || new PrismaClient({
+  log: ['query', 'error', 'warn'],
+})
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
