d542ccf9bf68b3a748363f6fcaca9a1dbf3025e9
Andy15084 <your-email@example.com>
2025-03-31 22:16:04 +0200

    Update: Configure Prisma for edge deployment with Accelerate

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma || new PrismaClient()
+export const prisma = globalThis.prisma || new PrismaClient().$extends(withAccelerate())
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
-
-export { prisma } 
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma 
