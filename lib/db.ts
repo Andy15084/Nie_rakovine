4991f26e72b8bf0b1c20c6f0393cb1efe08edc12
Andy15084 <your-email@example.com>
2025-03-31 21:53:38 +0200

    Update: Use Prisma Accelerate connection string

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,10 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-const prisma = globalThis.prisma || new PrismaClient({
-  log: ['query', 'error', 'warn'],
-})
+const prisma = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
